4dc70c0dcf6df47929e588f4ecea70f789fed1a7
@ger619 <abolger254@gmail.com>
2025-08-10 13:46:46 +0300

    Add Linux-specific platform dependencies to `Gemfile.lock`
    - Included Linux-specific versions of `bcrypt_pbkdf`, `nokogiri`, `pg`, `tailwindcss-ruby`, and `thruster`.
    - Updated `PLATFORMS` section to include `x86_64-linux` for broader compatibility.

--- db/migrate/20250806114208_create_homes.rb
+++
@@ -1,9 +0,0 @@
-class CreateHomes < ActiveRecord::Migration[8.0]
-  def change
-    create_table :homes do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
---
+++ db/migrate/20250810103713_homes.rb
@@ -0,0 +1,9 @@
+class Homes < ActiveRecord::Migration[8.0]
+  def change
+    create_table :homes, id: :uuid  do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
