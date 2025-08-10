eadb8e5a31155ca456d563ece67651cade45fb6b
Nemwel-Boniface <nemwelboniface@outlook.com>
2025-08-10 18:48:59 +0300

    Update included:   - Create custom method that creates custom account number fpr each account created   - Update the clients controller to ensure client accounts are shows correctly   - Update the client show page to show the accounts for a client if the KYC is approved/ client has accounts

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -7,6 +7,7 @@ class ClientsController < ApplicationController
   end
 
   def show
+    @client_accounts = @client.accounts
   end
 
   def new
--- app/models/account.rb
+++ app/models/account.rb
@@ -3,9 +3,20 @@ class Account < ApplicationRecord
 
   validate :client_must_have_approved_kyc
 
+  before_create :assign_account_number
   private
 
   def client_must_have_approved_kyc
     errors.add(:base, "KYC must be approved before creating an account") unless client&.kyc_approved?
+  end
+
+  def assign_account_number
+    last_account = Account.order(:created_at).last
+    last_number = if last_account&.account_number&.match?(/^mlpacc-(\d+)$/)
+                    last_account.account_number.split('-').last.to_i
+                  else
+                    0
+                  end
+    self.account_number = "mlpacc-#{last_number + 1}"
   end
 end
