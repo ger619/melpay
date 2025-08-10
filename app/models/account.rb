d605ba2004190087022b2cabee5f3727e42f5b17
Nemwel-Boniface <nemwelboniface@outlook.com>
2025-08-10 17:06:01 +0300

    Update included:   - Add associations validations and custom methods that check the KYC_status for a client   - In the Accounts model, ensure that an account for a client cannot be created before a client is approved

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,11 @@
 class Account < ApplicationRecord
   belongs_to :client
+
+  validate :client_must_have_approved_kyc
+
+  private
+
+  def client_must_have_approved_kyc
+    errors.add(:base, "KYC must be approved before creating an account") unless client&.kyc_approved?
+  end
 end
