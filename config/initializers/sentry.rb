6a94a4699599492825fb5b14bece7c07ecfe4831
@ger619 <abolger254@gmail.com>
2025-08-10 18:45:06 +0300

    Update Sentry DSN in initializer for new project configuration

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,8 +1,9 @@
 Sentry.init do |config|
-  config.dsn = 'https://2b4d1adaf9a79d33aca4892fd3ed72d4@o4507601057808384.ingest.de.sentry.io/4509790950064208'
+  config.dsn = 'https://130aeded055c1ce973d044dcee1abff5@o4507601057808384.ingest.de.sentry.io/4509790951178320'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Add data like request headers and IP for users,
   # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
   config.send_default_pii = true
 end
+
